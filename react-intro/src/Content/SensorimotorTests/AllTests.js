c88e3acd7785eba7d8a687bee454968641e5824a
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2023-04-11 18:32:23 +0300

    added sound test

--- react-intro/src/Content/SensorimotorTests/AllTests.js
+++ react-intro/src/Content/SensorimotorTests/AllTests.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from "./SimpleTests.module.css"
+import TestPage from './SimpleTests/TestPage'
 // Тут должен быть импорт компонент тестов
 
 const AllTests = () => {
     return (
         <div className={styles.content}>
-            
+            <TestPage/>
         </div>
     )
 }
